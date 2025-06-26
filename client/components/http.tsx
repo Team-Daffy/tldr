5b7afa845e32e076fe89ca6bbcae0dba5acde8cb
ElectroSpark115 <pcs311325@gmail.com>
2025-06-26 16:20:10 -0500

    added functionality to each button and made response box and question box appear after the initial url is sent to api

--- client/components/http.tsx
+++ client/components/http.tsx
@@ -1,19 +1,42 @@
 import { useEffect, useState } from 'react';
-interface MyComponentProps {
-    showRes: boolean;
-  }
-const Http = () => {
 
-
-
+const Http = ({ showRes, setShowRes }) => {
   const [showSumRes, setShowSumRes] = useState(false);
   const [url, setUrl] = useState(null);
   const [response, setResponse] = useState(null);
+  const [error, setError] = useState(null);
 
-  const urlSendHandler = () => {
-    //this will send the url to the backend, get a response and set a state variable to show the box with the response
-    setResponse({/*response we get from fetch request to backend (api response)*/});
-    setShowSumRes(true);
+  type OpenaiRes = {
+    body: string;
+  };
+  //this is likely going to change, this is just for a bit of scaffolding
+
+  const urlSendHandler = async (): Promise<void> => {
+    //this will send the url to the backend, get a response and set a
+    //state variable to show the box with the response
+    try {
+      const res = await fetch('/api/ai', {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify({url}),
+      });
+
+      if (!res.ok) {
+        throw new Error('Something failed in urlSendHandler function');
+      }
+
+      const data: OpenaiRes = await res.json();
+
+      if (data.body.length === 0) {
+        throw new Error('Recieved info has no body in urlSendHandler');
+      }
+
+      setResponse(data.body);
+      setShowRes(true);
+    } catch (err) {
+      setError('Server was unable to send a response, try again');
+      console.log(err);
+    }
   };
 
   return (
@@ -27,9 +50,13 @@ const Http = () => {
       <button id='send_URL' onClick={urlSendHandler}>
         Gimme Summ
       </button>
-      <div id='openai_res'>
-        {response}
-      </div>
+      {showRes && (
+        <section id='openai_res_container' className='response-container'>
+          <div id='openai_res' className='response'>
+            {response}
+          </div>
+        </section>
+      )}
     </section>
   );
 };
